c78228bb2a874594d7f470db2236bf8173012899
Saloni Mahajan <saloni.mahajan99@gmail.com>
2025-03-09 19:44:36 +0530

    graceful error handling-middleware

--- src/app.js
+++ src/app.js
@@ -10,9 +10,15 @@ app.use("/test",(req, res)=>{  //app.use to handle any incoming request. the arr
 app.use("/hello",(req, res)=>{  //app.use to handle any incoming request. the arroe functon with parameters req, and res is a request handler function ka arrow function
 
     res.send("Hello! hello")
-})
+});
 
 
+ app.use("/",(err, req, res, next)=>{     //ALWAYSSSS at the end to to handle any unexpected error gracefully without leaking any info
+    if(err)
+    {
+        res.status(400).send("SOMETHING IS WRONG");
+    }
+ });
 app.listen(3000, ()=>{
     console.log("challl gaya server"); //this funtion will run if the server is successfully ctively listening
 }); //with the port to listen the incoming request on the server
