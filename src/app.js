46852fc96f5262e5828e4ef408af1080d0627bd8
Saloni Mahajan <saloni.mahajan99@gmail.com>
2025-03-09 21:39:09 +0530

    mongoose se database connected

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express= require("express"); //referencing to the code of express from the node modules
+const connectDB = require("./config/database");
 
 const app= express(); //we called the express fucntion in a way creating an express application 
 
+connectDB().then(()=>{
+    console.log("connected DB");
+})
 app.use("/test",(req, res)=>{  //app.use to handle any incoming request. the arroe functon with parameters req, and res is a request handler function ka arrow function
 
     res.send("Hello! test")
---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const mongoose= require("mongoose"); //after installing mongoose becoz not part of npm 
+const connectDB = async()=> {
+    mongoose.connect("mongodb+srv://saloni007:<saloni@DB26>@nodejs.76dl4.mongodb.net/?retryWrites=true&w=majority&appName=Nodejs"); //ye string mongodb cluster se aayi he!
+}
+connectDB.then(()=>
+{
+    console.log("DB connected successfully") //happy case
+})
+.catch((err)=>
+{
+    console.log("DB connection failed"); //sad case
+});
+
+module.exports = connectDB; //yaha se export karenge n mail file me import
