9a1dd5cda53d9295f8af2f65c95d07d61a4cd661
Saloni Mahajan <saloni.mahajan99@gmail.com>
2025-03-23 03:34:59 +0530

    View all connections

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -12,7 +12,8 @@ userRouter.get("/user/requests", userAuth, async(req,res)=>{
         const connectionreq= await Request.find({
             toUserId: loggedinUser._id,
             status:"interested"
-        }).populate("fromUserId","firstname lastname instagram age gender n");
+        }).populate("fromUserId","firstname lastname instagram age gender "); // foreignkey!!!!
+    
 
         res.json({
             message: "tara tara ye ri requestttss",
@@ -26,4 +27,25 @@ userRouter.get("/user/requests", userAuth, async(req,res)=>{
     }
 })
 
+userRouter.get("/user/connections", userAuth, async(req,res)=>{
+    try{
+    const loggedinUser=req.user;
+
+    const connections= await Request.find({
+      $or:[  {toUserId: loggedinUser._id,
+        status:"accepted"},
+        {fromUserId: loggedinUser._id,
+            status:"accepted"}
+    ]}).populate("fromUserId", "firstname lastname instagram age gender ");
+
+    res.json({
+        message:"all connections",
+        data: connections
+    });
+    }
+    catch(err)
+    {
+        res.status(400).send("Sorry"+ err);
+    }
+})
 module.exports=userRouter;
