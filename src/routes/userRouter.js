ced5ee1178a5b2d2a940d8beb02cbad867428507
Saloni Mahajan <saloni.mahajan99@gmail.com>
2025-03-25 15:06:00 +0530

    FEED API and Pagination

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,7 +2,9 @@
 const express= require("express");
 const userRouter= express.Router();
 const userAuth= require("../utils/Userauthentiction");
+const mongoose = require("mongoose");
 const Request= require("../models/Request");
+const User = require("../models/User");
 
 const USER_SAFE_DATA= "firstname lastname instagram age gender";
 
@@ -58,4 +60,53 @@ userRouter.get("/user/connections", userAuth, async(req,res)=>{
         res.status(400).send("Sorry"+ err);
     }
 })
+
+    userRouter.get("/feed", userAuth, async(req,res)=>{
+        try{
+
+            const loggedInUser= req.user;
+
+            const page= parseInt(req.query.page) || 1;
+            let limit= parseInt(req.query.limit) || 10;
+           limit = limit >50 ? 50: limit;
+            const skip = (page-1)*limit;
+
+        const connreq= await Request.find({
+            $or:[
+                {fromUserId: loggedInUser},
+                {toUserId:loggedInUser}
+            ]
+        })
+
+        const hideUsersFromFeed= new Set();
+
+        connreq.forEach((req)=>{
+        hideUsersFromFeed.add(req.fromUserId.toString());
+        hideUsersFromFeed.add(req.toUserId.toString())});
+
+
+        // const users = await User.find({
+        //     _id: { $nin: [Array.from(hideUsersFromFeed)], $ne: loggedInUser._id }
+        // }).select("firstname lastname instagram age gender");
+
+        const users = await User.find({
+            _id: { 
+                $nin: [...hideUsersFromFeed].map(id => new mongoose.Types.ObjectId(id)), // Convert to ObjectId
+                $ne: new mongoose.Types.ObjectId(loggedInUser._id) // Convert logged-in user ID
+            }
+        }).select("firstname lastname instagram age gender")
+        .skip(skip)
+        .limit (limit);
+
+        res.json({
+            message: "Here is your feed",
+            data: users
+        });
+
+
+        }
+        catch(err){
+            res.status(400).send("SOrrryy"+err);
+        }
+    })
 module.exports=userRouter;
